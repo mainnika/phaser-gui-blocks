79f75f4dd97adf0ee7f6fe0ad53d0392b7cbbcd1
mainnika <nikita@tokarch.uk>
2017-03-15 11:25:46 +0500

    simple signals emitter

---
+++ helpers/signals.ts
@@ -0,0 +1,30 @@
+'use strict';
+
+export { Signals }
+
+class Signals<E> {
+
+	private signals: { [on: number]: Phaser.Signal };
+
+	public constructor() {
+
+		this.signals = {};
+	}
+
+	public on(event: E, cb: Function): void {
+
+		const signal: Phaser.Signal = this.signals[event as any] = this.signals[event as any] || new Phaser.Signal();
+		signal.add(cb);
+	}
+
+	public emit(event: E, ...args: any[]): void {
+
+		const signal: Phaser.Signal = this.signals[event as any];
+
+		if (!signal) {
+			return;
+		}
+
+		signal.dispatch(...args);
+	}
+}
